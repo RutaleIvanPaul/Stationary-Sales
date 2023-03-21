a67b05b898db57000e0d82d7141c22fa6dd6ef6b
Adrian <adrian@ramani.io>
2023-03-21 09:33:42 -0500

    When press back gesture from home screen, the logout alert will be appeared

--- app/src/main/java/io/ramani/ramaniStationary/app/home/presentation/HomeHostFragment.kt
+++ app/src/main/java/io/ramani/ramaniStationary/app/home/presentation/HomeHostFragment.kt
@@ -168,7 +168,7 @@ class HomeHostFragment : BaseFragment() {
     }
 
     override fun onBackButtonPressed(): Boolean {
-        // When press back button on home page, the login alert will be shown and no need to back from stack
+        // When press back button on home page, the logout alert will be shown and no need to back from stack
         //flow.onBackPressed()
         showConfirmDialog(getString(R.string.confirm_logout), onConfirmed = {
             viewModel.logout {
