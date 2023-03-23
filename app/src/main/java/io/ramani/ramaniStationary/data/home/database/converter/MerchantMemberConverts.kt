9f22715ac1b1735f8b38a96f8970879296352eb4
Adrian <adrian@ramani.io>
2023-03-23 03:26:22 -0500

    Add converter to save list types to database

--- app/src/main/java/io/ramani/ramaniStationary/data/home/database/converter/MerchantMemberConverts.kt
+++ app/src/main/java/io/ramani/ramaniStationary/data/home/database/converter/MerchantMemberConverts.kt
@@ -2,12 +2,13 @@ package io.ramani.ramaniStationary.data.home.database.converter
 
 import androidx.room.TypeConverter
 import com.google.gson.Gson
+import io.ramani.ramaniStationary.domain.home.model.MerchantMemberModel
 import io.ramani.ramaniStationary.domain.home.model.ProductCategoryModel
 
-class ProductCategoryConverts {
+class MerchantMemberConverts {
     @TypeConverter
-    fun listToJson(value: List<ProductCategoryModel>?) = Gson().toJson(value)
+    fun listToJson(value: List<MerchantMemberModel>?) = Gson().toJson(value)
 
     @TypeConverter
-    fun jsonToList(value: String) = Gson().fromJson(value, Array<ProductCategoryModel>::class.java).toList()
+    fun jsonToList(value: String) = Gson().fromJson(value, Array<MerchantMemberModel>::class.java).toList()
 }
--- app/src/main/java/io/ramani/ramaniStationary/data/home/database/converter/ProductCategoryConverts.kt
+++ app/src/main/java/io/ramani/ramaniStationary/data/home/database/converter/ProductCategoryConverts.kt
@@ -1,11 +1,13 @@
-package io.ramani.ramaniStationary.data.home.database
+package io.ramani.ramaniStationary.data.home.database.converter
 
 import androidx.room.TypeConverter
+import com.google.gson.Gson
+import io.ramani.ramaniStationary.domain.home.model.ProductCategoryModel
 
 class ProductCategoryConverts {
     @TypeConverter
-    fun listToJson(value: List<JobWorkHistory>?) = Gson().toJson(value)
+    fun listToJson(value: List<ProductCategoryModel>?) = Gson().toJson(value)
 
     @TypeConverter
-    fun jsonToList(value: String) = Gson().fromJson(value, Array<JobWorkHistory>::class.java).toList()
+    fun jsonToList(value: String) = Gson().fromJson(value, Array<ProductCategoryModel>::class.java).toList()
 }
